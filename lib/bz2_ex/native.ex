4319d82cce299f29a35879ecd15dafa0378fdf6f
HeroesLament <heroeslament@nunyabiz.net>
2026-01-27 17:28:00 -0900

    Unify repo syntax

--- lib/bz2_ex/native.ex
+++ lib/bz2_ex/native.ex
@@ -1,24 +1,24 @@
 defmodule Bz2Ex.Native do
   @moduledoc false
 
-  version = Mix.Project.config()[:version]
+  @version Mix.Project.config()[:version]
 
   use RustlerPrecompiled,
     otp_app: :bz2_ex,
     crate: "bz2_ex",
-    base_url: "https://github.com/HeroesLament/bz2-ex/releases/download/v#{version}",
+    base_url: "https://github.com/HeroesLament/bz2_ex/releases/download/v#{@version}",
     force_build: System.get_env("BZ2_EX_BUILD") in ["1", "true"],
+    nif_versions: ["2.16"],
     targets: ~w(
       aarch64-apple-darwin
+      x86_64-apple-darwin
       aarch64-unknown-linux-gnu
       aarch64-unknown-linux-musl
-      x86_64-apple-darwin
       x86_64-unknown-linux-gnu
       x86_64-unknown-linux-musl
-      x86_64-pc-windows-msvc
-      x86_64-pc-windows-gnu
+      x86_64-unknown-freebsd
     ),
-    version: version
+    version: @version
 
   def compress(_input, _block_size, _work_factor), do: :erlang.nif_error(:nif_not_loaded)
   def decompress(_input, _small), do: :erlang.nif_error(:nif_not_loaded)
